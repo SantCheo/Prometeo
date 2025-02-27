07fd8893416c99b2cec004787a39b7a9556d504b
SantCheo <alfredoceiba@gmail.com>
2025-02-27 15:04:33 +0100

    cambios 27/02

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -41,5 +41,91 @@
 # 	else:
 # 		print("Año Bisiesto")
 
- 
 
+# 
+#  Almacena el actual número más grande aquí.
+# largest_number = -999999999
+ 
+# Ingresa el primer valor.
+# number = int(input("Introduce un número o escribe -1 para detener: "))
+ 
+# Si el número no es igual a -1, continuaremos
+# while number != -1:
+#     ¿Es el número más grande que el valor de largest_number?
+#     if number > largest_number:
+#         Sí si, se actualiza largest_number.
+#         largest_number = number
+#     Ingresa el siguiente número.
+#     number = int(input("Introduce un número o escribe -1 para detener: "))
+ 
+# Imprime el número más grande.
+# print("El número más grande es:", largest_number)
+
+
+# Un programa que lee una secuencia de números
+# y cuenta cuántos números son pares y cuántos son impares.
+# El programa termina cuando se ingresa un cero.
+
+# odd_numbers = 0
+# even_numbers = 0
+
+# # Lee el primer número.
+# number = int(input("Introduce un número o escribe 0 para detener: "))
+
+# # 0 termina la ejecución.
+# while number:
+#     # Verificar si el número es impar.
+#     if number % 2:
+#         # Incrementar el contador de números impares odd_numbers.
+#         odd_numbers += 1
+#     else:
+#         # Incrementar el contador de números pares even_numbers.
+#         even_numbers += 1
+#     # Leer el siguiente número.
+#     number = int(input("Introduce un número o escribe 0 para detener: "))
+
+# # Imprimir resultados.
+# print("Conteo de números impares:", odd_numbers)
+# print("Conteo de números pares:", even_numbers)
+
+# counter = 5
+# while counter != 0:
+#     print("Dentro del bucle.", counter)
+#     counter -= 1
+# print("Fuera del bucle.", counter)
+
+# secret_number = 777
+
+# print(
+# """
+# +================================+
+# | ¡Bienvenido a mi juego, muggle!|
+# | Introduce un número entero     |
+# | y adivina qué número he        |
+# | elegido para ti.               |
+# |¿Cuál es el número secreto?     |
+# +================================+
+# """)
+# numero= int ( input (' ingrese un número entero'))
+
+# while secret_number != numero:
+#             print ("¡Ja, ja! ¡Estás atrapado en mi bucle!")
+#             numero= int ( input (' ingrese un número entero'))
+# print ('¡Bien hecho, muggle! Eres libre ahora.')
+
+
+# largest_number = -99999999
+# counter = 0
+
+# while True:
+#     number = int(input("Ingresa un número o escribe -1 para finalizar el programa: "))
+#     if number == -1:
+#         break
+#     counter += 1
+#     if number > largest_number:
+#         largest_number = number
+
+# if counter != 0:
+#     print("El número más grande es", largest_number)
+# else:
+#     print("No has ingresado ningún número.")
