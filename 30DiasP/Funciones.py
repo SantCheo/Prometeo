9cd9141fd53e01a45b0a69435daa5bc83d6b39d6
SantCheo <alfredoceiba@gmail.com>
2025-03-02 18:13:12 +0100

    cambios 02/03/25

---
+++ 30DiasP/Funciones.py
@@ -0,0 +1,22 @@
+# def hola():
+#     print ("Hola mundo!")
+#     print ("Bienvenido")
+
+# hola()
+# hola()
+
+# def hola(nombre):
+#     print ("Hola mundo!")
+#     print (f"Bienvenido {nombre}")
+
+# hola("Alfredo") 
+# hola("Chanchito Feliz")
+  
+
+# VARIOS PARAMETROS
+def hola(nombre, apellido):
+    print ("Hola mundo!")
+    print (f"Bienvenido {nombre}{apellido}")
+
+hola("Alfredo", "Rivas") 
+hola("Chanchito Feliz", "y rechoncho")
